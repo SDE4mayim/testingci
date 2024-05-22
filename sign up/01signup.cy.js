5a9bc3361c08e3b27f7685f2c14f05f68e9accc3
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-05-22 15:47:33 +0530

    Delete sign up directory

--- sign up/01signup.cy.js
+++
@@ -1,9 +0,0 @@
-describe('open',() =>{
-    it('verify',() =>{
-        cy.visit('/')
-        cy.get(".btn-sm").click()
-        cy.title().should("eq","VET CASTLE")
-        cy.url().should("eq","https://vetcastle.com/signup")
-    })
-}
-)
