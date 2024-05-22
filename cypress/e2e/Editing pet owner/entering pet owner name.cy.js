bbd0d15e4bb92b881db9e7757ba7766c9dbaad96
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-05-22 15:36:41 +0530

    Add files via upload

---
+++ cypress/e2e/Editing pet owner/entering pet owner name.cy.js
@@ -0,0 +1,30 @@
+describe('open',() =>{
+    it('verify',() =>{
+       cy.visit('/')
+       cy.get('.grow > :nth-child(2) > .font-medium').click()  
+       cy.url().should('eq','https://vetcastle.com/signin') 
+        cy.get('.justify-center > :nth-child(3)').click()
+        cy.get('.w-full > .text-gray-400').contains("Hospitals and clinics can use this login")
+        cy.get('#username').type("srivathsandv2")
+        cy.get('#password').type("1SDE@mayim")
+        cy.get('.btn').click()
+       cy.wait(1000)
+       cy.visit("/maindashboard")
+       cy.get('[href="/csd"] > .MuiListItemText-root > .MuiTypography-root').click()
+       cy.url().should('eq','https://vetcastle.com/csd')
+       cy.get('#search').type("Krish")
+       cy.get('.MuiGrid-container > :nth-child(2) > .MuiButtonBase-root').click()
+       
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ cypress/e2e/editing doctor/entering email.cy.js
@@ -0,0 +1,22 @@
+describe('open',() =>{
+    it('verify',() =>{
+       cy.visit('/')
+       cy.get('.grow > :nth-child(2) > .font-medium').click()
+       cy.url().should('eq','https://vetcastle.com/signin')
+       cy.get('.w-full > .text-gray-400').should('contain','Pet Owners can use this login')
+       cy.get('.justify-center > :nth-child(3)').click()
+       cy.get('#username').type('srivathsandv2')
+       cy.get('#password').type('1SDE@mayim')
+       cy.get('.btn').click()
+       cy.wait(1000)
+       cy.reload()
+       cy.visit("/maindashboard")
+       cy.get('[href="/dsd"] > .MuiListItemText-root > .MuiTypography-root').click()
+       cy.url().should('eq',"/dsd")
+       cy.get('[data-id="300001"] > [data-field="action"] > .cellAction > .viewbutton').click()
+       cy.get('[data-testid="EditIcon"]').click()
+       cy.get(':nth-child(2) > :nth-child(9)').clear().type("sjchristo2gmail.com")//clear the mail and entering new email
+       cy.get(':nth-child(2) > :nth-child(9)').type("sjchristo@gmail.com")//entering email with existing email
+       cy.get('.MuiDialogActions-root > .MuiGrid-container > :nth-child(2) > :nth-child(1)').click()
+    })
+})
