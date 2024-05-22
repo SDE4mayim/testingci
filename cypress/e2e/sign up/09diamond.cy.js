e738bf8bcd6769d0220a5235700e77e9cf9c7b63
SDE4mayim <162966695+SDE4mayim@users.noreply.github.com>
2024-05-22 15:48:31 +0530

    Add files via upload

---
+++ cypress/e2e/sign up/09diamond.cy.js
@@ -0,0 +1,14 @@
+describe('logo',() => {
+    it('logo',()  =>{
+        cy.visit('/')
+        cy.get(".grow > :nth-child(1) > .font-medium").click()
+        cy.title().should("eq","VET CASTLE")
+        cy.url().should("eq","https://vetcastle.com/pricing")
+        cy.get(':nth-child(2) > .btn-sm').click()
+        cy.url().should("eq","https://vetcastle.com/dsignup")
+        cy.get(".text-gray-400").should("contain.text","Rs.3,500 per month")
+        cy.get(".text-red-400").should("contain.text","Special Offer: 50% Discount on Yearly Plans. Pay Only Rs. 21,000 Annually (Originally Rs. 42,000)")
+        
+    })
+ })
+ 
---
+++ cypress/e2e/sign up/35enteringmorenoinphno.cy.js
@@ -0,0 +1,26 @@
+describe('open',() =>{
+    it('verify',() =>{
+        cy.visit('/')
+        cy.get(".btn-sm").click()
+        cy.title().should("eq","VET CASTLE")
+        cy.url().should("eq","https://vetcastle.com/signup")
+        cy.get(".border-b-2").click()
+        cy.get("#username").type("_ddvv_srivathsan_s")
+        cy.get("#first-name").type("Srivathsan!")
+        cy.get("#last-name").type("D V")
+        cy.get('#hospitalCategory').select("Private Practices")
+        cy.get(":nth-child(6) > .w-full > .react-tel-input > .form-control").type("93605116170")
+        cy.get(":nth-child(7) > .w-full > .react-tel-input > .form-control").type("9658745632")
+        cy.get("#email").type("sdemayim1@gmail.com")
+        cy.get("#password").type("DVSrivathsan")
+        cy.get("#address").type("TNHB, VIRUPATCHIPURAM")
+        cy.get("#country").select("India")
+        cy.get("#state").select("Tamil Nadu")
+        cy.get("#city").select("Dharmapuri")
+        cy.get("#pincode").type("636705")
+        cy.get('.btn').click()
+        cy.get('.MuiAlert-message').should("contain.text","Error creating Hospital Profile : Profile creation error")
+        //onlytakingfirst10numbers
+    })
+}
+)
